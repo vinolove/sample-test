e3e8aca43a87ce3a2a56ace1263caca362ef3932
vinolove <vinothkumar@clevero.com>
2018-05-08 14:56:10 +0530

    normal code to check jenkins

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var http = require("http");
+var http = require('http');
 
 http.createServer(function(req,res){
-    res.end("This is server")
+    res.end("This is vino's local");
+}).listen(8085);
